9bf11d71076058777ff51a5fa26c3c1ed1cecd6b
93rohit <rohit123.friends9@gmail.com>
2024-08-02 21:03:05 +0530

    index.html file added

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown', (event) => {
+    const key = event.key === ' ' ? 'Space' : event.key;
+    const keycode = event.keyCode || event.which;
+    
+    document.getElementById('key').textContent = key;
+    document.getElementById('keycode').textContent = keycode;
+  });
+  
